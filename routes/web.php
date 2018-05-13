445caf95315217333d2f8b33427f139c9cf833e2
Shmully Wolfson <shmullywolfson@gmail.com>
2018-05-13 15:11:54 -0400

    add to composer.json

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
-use App\Post;
 
+
+Route::get('/', 'PostsController@index');
 Route::get('/posts', 'PostsController@index');
-
-Route::get('/posts', function () {
-
-  // $posts = DB::table('posts')->latest()->get();
-  $posts = Post::all();
-
-    return view('posts.index', compact('posts'));
-});
-
-Route::get('/posts/{post}', function($id){
-  // $post = DB::table('posts')->find($id);
-  $post = Post::find($id);
-  return view('posts.show', compact('post'));
-});
-
-Route::get('/', function () {
-  $posts = DB::table('posts')->latest()->get();
-
-    return view('posts.index', compact('posts'));
-});
+Route::get('/posts/{post}', 'PostsController@show');
