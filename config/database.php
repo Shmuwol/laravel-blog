5d6ed6758b721af867f1970d4229dc97de4cc051
Shmully Wolfson <shmullywolfson@gmail.com>
2018-05-14 18:57:05 -0400

    add env variables

--- config/database.php
+++ config/database.php
@@ -1,5 +1,15 @@
 <?php
 
+$host = $port = $username = $password = $database = '';
+if(getenv("CLEARDB_DATABASE_URL")){
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $post = $url["port"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
+
 return [
 
     /*
@@ -43,9 +53,9 @@ return [
           'driver' => 'mysql',
           'host' => env('DB_HOST', $host),
           'port' => env('DB_PORT', '3306'),
-          'database' => env('DB_DATABASE', 'larapost'),
-          'username' => env('DB_USERNAME', 'root'),
-          'password' => env('DB_PASSWORD', ''),
+          'database' => env('DB_DATABASE', $database),
+          'username' => env('DB_USERNAME', $username),
+          'password' => env('DB_PASSWORD', $password),
           'unix_socket' => env('DB_SOCKET', ''),
           'charset' => 'utf8mb4',
           'collation' => 'utf8mb4_unicode_ci',
